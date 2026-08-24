91a30f7d14ade01c07cc089aa3f0c77e6167c604
wza2001 <wza2001@icloud.com>
2026-08-24 10:46:56 +0400

    feat: integrate jules changes and add homework view

---
+++ src/views/Homework.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ViewMap from "@/components/ViewMap.vue";
+import SpatialChart from "@/components/SpatialChart.vue";
+</script>
+<template>
+    <div class="Page Root">
+
+      <div class="first_parts">
+        <div class="card">
+          <SpatialChart :chart-option="pieOption" />
+        </div>
+        <div class="card">
+          <SpatialChart :chart-option="barOption" />
+      </div>
+    </div>
+</template>
